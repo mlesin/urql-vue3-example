5e0bdd19f0d43fa3dd79baaa1f259db7293a0203
Mikhail Lesin <mlesin@mrpost.ru>
2022-11-20 20:24:22 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Suspense>
+    <template #default>
+      <PokemonList />
+    </template>
+    <template #fallback>
+      <div>Loading...</div>
+    </template>
+  </Suspense>
+</template>
+
+<script>
+import { defaultExchanges, provideClient } from "@urql/vue";
+import PokemonList from "./PokemonList.vue";
+import { devtoolsExchange } from "@urql/devtools";
+
+export default {
+  name: "App",
+  setup() {
+    provideClient({
+      url: "https://trygql.formidable.dev/graphql/basic-pokedex",
+      exchanges: [devtoolsExchange, ...defaultExchanges],
+    });
+  },
+  components: {
+    PokemonList,
+  },
+};
+</script>
